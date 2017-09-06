15631cf04221b9440d3e3cb58e5614fc58b99b11
Giacomo <giacomo.licari@gmail.com>
2017-09-06 12:04:20 +0200

    Added gas price selector

--- src/selectors/blockchain.js
+++ src/selectors/blockchain.js
@@ -6,6 +6,8 @@ export const getDefaultAccount = state => selector(state).defaultAccount
 
 export const getGasCosts = state => state.blockchain.gasCost
 
+export const getGasPrice = state => state.blockchain.gasPrice
+
 export default {
   getDefaultAccount,
 }
