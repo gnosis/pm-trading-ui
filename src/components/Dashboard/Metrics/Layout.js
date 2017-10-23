9ba87145923ea5a0be368449b2b7c58c8dce7f11
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-10-23 17:47:40 +0200

    PM-90 Fetching data from selectors for Dashboard's Metrics

--- src/components/Dashboard/Metrics/Layout.js
+++ src/components/Dashboard/Metrics/Layout.js
@@ -10,9 +10,14 @@ const arrows = require('./assets/arrows.png')
 const group = require('./assets/group.png')
 const shape = require('./assets/shape.png')
 
-const Metric = ({ img, title, explanation }) => (
+const Metric = ({ img, title, explanation, width, height }) => (
     <Block className={ cx('ol-db-metric') }>
-        <Img className={ cx('ol-db-icon') } src={ img } width="42" height="42" />
+        <Img
+            className={ cx('ol-db-icon') }
+            src={ img }
+            width={ width ? width : 37 }
+            height={ width ? width : 37 }
+        />
         <Block>
             <Block className={ cx('ol-db-title') }>{ title ? title : '--' }</Block>
             <Block className={ cx('ol-db-explanation') }>{ explanation }</Block>
@@ -23,7 +28,7 @@ const Metric = ({ img, title, explanation }) => (
 const Metrics = ({ tokens, predictedProfits, rank }) => (
     <Block className={ cx('ol-db-container') }>
         <Metric img={ group } title={ tokens } explanation="OLY TOKENS" />
-        <Metric img={ shape } title={ predictedProfits } explanation="PREIDCTED PROFITS" />
+        <Metric img={ shape } width={ 45 } height={ 45 } title={ predictedProfits } explanation="PREDICTED PROFITS" />
         <Metric img={ arrows} title={ rank } explanation="YOUR RANK" />
     </Block>
 )
--- src/components/Dashboard/Metrics/index.js
+++ src/components/Dashboard/Metrics/index.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { connect } from 'react-redux'
 import Layout from './Layout'
-
+import selector from './selector'
 class Metrics extends React.PureComponent {
 
     render() {
+        const { tokens, predictedProfits, rank } = this.props
+        
         return (
-            <Layout tokens="200" predictedProfits={ -32 } rank={ 23 } />
+            <Layout tokens={ tokens } predictedProfits={ predictedProfits } rank={ rank } />
         );
     }
 }
 
-export default connect()(Metrics)
+export default connect(selector)(Metrics)
