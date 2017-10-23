2426c682aa7eef65534b727eb6c7e1135d7817b8
mmv <Mikhail Mikheev>
2017-10-23 18:15:15 +0400

    start transactions explanations

---
+++ src/utils/transactionExplanations.js
@@ -0,0 +1,6 @@
+export const BUY_SHARES = [
+  "We will ask you to approve depositing to this market's contract",
+  'We will ask you to approve a transaction of outcome tokens',
+]
+
+export const SETTING_ALLOWANCE = ['We will ask you to approve setting allowance']
