9fed7856c85db082237a1f820b826728b193094b
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-10-24 08:20:14 +0200

    PM-90 Default width on icons in function var declaration

--- src/components/Dashboard/Metrics/Layout.js
+++ src/components/Dashboard/Metrics/Layout.js
@@ -10,13 +10,13 @@ const arrows = require('./assets/arrows.png')
 const group = require('./assets/group.png')
 const shape = require('./assets/shape.png')
 
-const Metric = ({ img, title, explanation, width, height }) => (
+const Metric = ({ img, title, explanation, width = 37, height = 37 }) => (
     <Block className={ cx('ol-db-metric') }>
         <Img
             className={ cx('ol-db-icon') }
             src={ img }
-            width={ width ? width : 37 }
-            height={ width ? width : 37 }
+            width={ width }
+            height={ width }
         />
         <Block>
             <Block className={ cx('ol-db-title') }>{ title ? title : '--' }</Block>
