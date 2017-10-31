671fd757b6a1556d0d5af12783dd8b779e7488d5
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-10-31 13:47:30 +0100

    PM-113 Formatting OLY tokens

--- src/routes/scoreboard/components/ScoreTable/table.js
+++ src/routes/scoreboard/components/ScoreTable/table.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind'
 import * as React from 'react'
-import { roundProfits } from 'utils/helpers';
+import { weiToEth } from 'utils/helpers';
 
 import * as css from './index.css'
 
@@ -46,7 +46,7 @@ export const rankCell = (props) => {
 
 export const olyCell = prop => (props) => {
     const value = props.row[prop]
-    const result = roundProfits(value)
+    const result = weiToEth(value)
     return <span>{ result }</span>
 }
 
