d4b2a4ecedadf75125ae2ce1aa417742d04efb39
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-11-01 11:26:47 +0100

    PM-102 Adding storybook Hairline component

---
+++ src/components/layout/Hairline/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const hairlineStyle = {
+    width: '100%',
+    height: '2px',
+    backgroundColor: '#e7e7e7',
+}
+
+const Hairline = () => (
+  <div style={hairlineStyle} />
+)
+
+export default Hairline
