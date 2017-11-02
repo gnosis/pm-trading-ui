e81521ae57b4844d892a28753588e29a0c7c2f26
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-11-02 15:46:46 +0100

    Implemented general changes on layout components using semantic description for gameguide

--- src/routes/gamerules/components/SignUp/index.js
+++ src/routes/gamerules/components/SignUp/index.js
@@ -7,7 +7,7 @@ const SignUp = () => (
   <Block margin="xl">
     <Subtitle>Sign-up</Subtitle>
     <Paragraph color="medium">
-        To participate in our two-week tournament, verify your identity by signing up via the uPort app on Android or iOS. <br />
+        To participate in our two-week tournament, verify your identity by signing up via the uPort app on Android or iOS. <br /><br />
         uPort is a secure, easy-to-use system for self-sovereign identity, built on Ethereum. uPort identities are fully
          owned and controlled by the creator, and don't rely on centralized third-parties for creation or validation.
           Via uPort, you can own and control your personal identity; securely and selectively disclose your data to counterparties, and interact with decentralized applications and smart contracts such as Gnosis.
