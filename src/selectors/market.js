a454269a13de75640351939f4bb4a317b9333659
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-11-03 16:21:13 +0100

    PM-134 Change order

--- src/selectors/market.js
+++ src/selectors/market.js
@@ -7,67 +7,67 @@ import { getEventDescriptionByAddress } from './eventDescription'
 
 
 export const getMarketById = state => (marketAddress) => {
-    const marketEntities = entitySelector(state, 'markets')
+  const marketEntities = entitySelector(state, 'markets')
 
-    let market = {}
-    if (marketEntities[marketAddress]) {
-        const marketEntity = marketEntities[marketAddress]
+  let market = {}
+  if (marketEntities[marketAddress]) {
+    const marketEntity = marketEntities[marketAddress]
 
-        const marketEvent = getEventByAddress(state)(marketEntity.event)
+    const marketEvent = getEventByAddress(state)(marketEntity.event)
 
-        if (!marketEvent) {
-            return market
-        }
-
-        const eventOracle = getOracleByAddress(state)(marketEvent.oracle)
-
-        if (!eventOracle) {
-            return market
-        }
-
-        const oracleEventDescription =
-      getEventDescriptionByAddress(state)(eventOracle.eventDescription)
-
-        if (!oracleEventDescription) {
-            return market
-        }
-
-        market = {
-            ...marketEntities[marketAddress],
-            event: marketEvent,
-            oracle: eventOracle,
-            eventDescription: oracleEventDescription,
-        }
+    if (!marketEvent) {
+      return market
     }
 
-    return market
+    const eventOracle = getOracleByAddress(state)(marketEvent.oracle)
+
+    if (!eventOracle) {
+      return market
+    }
+
+    const oracleEventDescription =
+      getEventDescriptionByAddress(state)(eventOracle.eventDescription)
+
+    if (!oracleEventDescription) {
+      return market
+    }
+
+    market = {
+      ...marketEntities[marketAddress],
+      event: marketEvent,
+      oracle: eventOracle,
+      eventDescription: oracleEventDescription,
+    }
+  }
+
+  return market
 }
 
 export const getMarketShareByShareId = state => (shareAddress) => {
-    const marketShareEntities = entitySelector(state, 'marketShares')
+  const marketShareEntities = entitySelector(state, 'marketShares')
 
-    return marketShareEntities[shareAddress]
+  return marketShareEntities[shareAddress]
 }
 
 export const getMarkets = (state) => {
-    const marketEntities = entitySelector(state, 'markets')
+  const marketEntities = entitySelector(state, 'markets')
 
-    return Object.keys(marketEntities).map(getMarketById(state))
+  return Object.keys(marketEntities).map(getMarketById(state))
 }
 
 export const getMarketSharesByMarket = state => (marketAddress) => {
-    const marketEntity = getMarketById(state)(marketAddress)
-    const shares = get(marketEntity, 'shares', [])
+  const marketEntity = getMarketById(state)(marketAddress)
+  const shares = get(marketEntity, 'shares', [])
 
-    return shares.map(shareAddress => getMarketShareByShareId(state)(shareAddress)).filter(share => share.balance > 0)
+  return shares.map(shareAddress => getMarketShareByShareId(state)(shareAddress)).filter(share => share.balance > 0)
 }
 
 export const filterMarkets = state => (opts) => {
-    const marketEntities = getMarkets(state)
+  const marketEntities = getMarkets(state)
 
-    const { textSearch, resolved, onlyMyMarkets, onlyModeratorsMarkets, defaultAccount } = opts
+  const { textSearch, resolved, onlyMyMarkets, onlyModeratorsMarkets, defaultAccount } = opts
 
-    return marketEntities
+  return marketEntities
     .filter(market =>
       (!textSearch ||
         market.eventDescription.title.toLowerCase().indexOf(textSearch.toLowerCase()) > -1 ||
@@ -84,28 +84,28 @@ export const filterMarkets = state => (opts) => {
  * @param {*} orderBy, orderBy criteria
  */
 export const sortMarkets = (markets = [], orderBy = null) => {
-    switch (orderBy) {
-        case 'RESOLUTION_DATE_ASC':
-            return markets.sort((a, b) => a.eventDescription.resolutionDate > b.eventDescription.resolutionDate)
-        case 'RESOLUTION_DATE_DESC':
-            return markets.sort((a, b) => a.eventDescription.resolutionDate < b.eventDescription.resolutionDate)
-        case 'TRADING_VOLUME_ASC':
-            return markets.sort((a, b) => {
-                const tradingA = Decimal(a.tradingVolume).div(1e18).toDP(2, 1)
-                const tradingB = Decimal(b.tradingVolume).div(1e18).toDP(2, 1)
+  switch (orderBy) {
+  case 'RESOLUTION_DATE_ASC':
+    return markets.sort((a, b) => a.eventDescription.resolutionDate > b.eventDescription.resolutionDate)
+  case 'RESOLUTION_DATE_DESC':
+    return markets.sort((a, b) => a.eventDescription.resolutionDate < b.eventDescription.resolutionDate)
+  case 'TRADING_VOLUME_DESC':
+    return markets.sort((a, b) => {
+      const tradingA = Decimal(a.tradingVolume).div(1e18).toDP(2, 1)
+      const tradingB = Decimal(b.tradingVolume).div(1e18).toDP(2, 1)
 
-                return tradingA.comparedTo(tradingB)
-            })
-        case 'TRADING_VOLUME_DESC':
-            return markets.sort((a, b) => {
-                const tradingA = Decimal(a.tradingVolume).div(1e18).toDP(2, 1)
-                const tradingB = Decimal(b.tradingVolume).div(1e18).toDP(2, 1)
+      return tradingB.comparedTo(tradingA)
+    })
+  case 'TRADING_VOLUME_ASC':
+    return markets.sort((a, b) => {
+      const tradingA = Decimal(a.tradingVolume).div(1e18).toDP(2, 1)
+      const tradingB = Decimal(b.tradingVolume).div(1e18).toDP(2, 1)
 
-                return tradingB.comparedTo(tradingA)
-            })
-        default:
-            return markets
-    }
+      return tradingA.comparedTo(tradingB)
+    })
+  default:
+    return markets
+  }
 }
 
 /**
@@ -113,12 +113,12 @@ export const sortMarkets = (markets = [], orderBy = null) => {
  * @param {*} state
  */
 export const getMarketParticipantsTrades = state => () => {
-    const tradesArray = []
-    const tradesObject = state.entities.trades
-    if (tradesObject) {
-        Object.keys(state.entities.trades).map(key => tradesArray.push(tradesObject[key]))
-    }
-    return tradesArray
+  const tradesArray = []
+  const tradesObject = state.entities.trades
+  if (tradesObject) {
+    Object.keys(state.entities.trades).map(key => tradesArray.push(tradesObject[key]))
+  }
+  return tradesArray
 }
 
 /**
@@ -127,8 +127,8 @@ export const getMarketParticipantsTrades = state => () => {
  * @param {String} account, an address
  */
 export const getAccountShares = (state, account) => {
-    const accountShares = entitySelector(state, 'accountShares')
-    return accountShares[account] ? accountShares[account].shares : []
+  const accountShares = entitySelector(state, 'accountShares')
+  return accountShares[account] ? accountShares[account].shares : []
 }
 
 /**
@@ -137,42 +137,42 @@ export const getAccountShares = (state, account) => {
  * @param {String} account, an address
  */
 export const getAccountTrades = (state, account) => {
-    const accountTrades = entitySelector(state, 'accountTrades')
-    return accountTrades[account] ? accountTrades[account].trades : []
+  const accountTrades = entitySelector(state, 'accountTrades')
+  return accountTrades[account] ? accountTrades[account].trades : []
 }
 
 export const getAccountPredictiveAssets = (state, account) => {
-    let predictiveAssets = new Decimal(0)
+  let predictiveAssets = new Decimal(0)
 
-    if (account) {
-        const shares = getAccountShares(state, account)
-        if (shares.length) {
-            predictiveAssets = shares.reduce(
+  if (account) {
+    const shares = getAccountShares(state, account)
+    if (shares.length) {
+      predictiveAssets = shares.reduce(
         (assets, share) => assets.add(new Decimal(share.balance).mul(share.marginalPrice)), new Decimal(0),
       )
-        }
     }
-    return predictiveAssets
+  }
+  return predictiveAssets
 }
 
 export const getAccountParticipatingInEvents = (state, account) => {
-    const events = []
+  const events = []
 
-    if (account) {
-        const shares = getAccountShares(state, account)
+  if (account) {
+    const shares = getAccountShares(state, account)
 
-        if (shares.length) {
-            shares.map((share) => {
-                if (events.indexOf(share.outcomeToken.event) === -1) {
-                    events.push(share.outcomeToken.event)
-                }
-                return events
-            })
+    if (shares.length) {
+      shares.map((share) => {
+        if (events.indexOf(share.outcomeToken.event) === -1) {
+          events.push(share.outcomeToken.event)
         }
+        return events
+      })
     }
-    return events
+  }
+  return events
 }
 
 export default {
-    getMarkets,
+  getMarkets,
 }
