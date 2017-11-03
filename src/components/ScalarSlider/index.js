a0d5e0601e767fa3ce8fa56d3a1d1af61803bca8
Andre Meyer <andre.meyer93@gmail.com>
2017-11-03 14:34:03 +0100

    introduce pin left/right if too far on one side, fixes display issues, add flexbox

--- src/components/ScalarSlider/index.js
+++ src/components/ScalarSlider/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Decimal from 'decimal.js'
+import cn from 'classnames'
 
 import DecimalValue from 'components/DecimalValue'
 
@@ -14,54 +15,59 @@ const ScalarSlider = ({
   marginalPriceSelected,
   decimals,
 }) => {
-  const bigLowerBound = new Decimal(lowerBound)
-  const bigUpperBound = new Decimal(upperBound)
+    const bigLowerBound = new Decimal(lowerBound)
+    const bigUpperBound = new Decimal(upperBound)
 
   // current value
-  const bounds = bigUpperBound.sub(bigLowerBound).div(10 ** decimals)
+    const bounds = bigUpperBound.sub(bigLowerBound).div(10 ** decimals)
 
-  const value = new Decimal(marginalPriceCurrent).mul(bounds.toString()).add(bigLowerBound.div(10 ** decimals).toString())
-  const percentage = new Decimal(marginalPriceCurrent).mul(100)
+    const value = new Decimal(marginalPriceCurrent).mul(bounds.toString()).add(bigLowerBound.div(10 ** decimals).toString())
+    const percentage = new Decimal(marginalPriceCurrent).mul(100)
 
-  const selectedValue = new Decimal(marginalPriceSelected).mul(bounds.toString()).add(bigLowerBound.div(10 ** decimals).toString())
-  const selectedPercentage = new Decimal(marginalPriceSelected).mul(100)
-  return (
-    <div className="scalarSlider">
-      <div className="slider">
-        <div className="slider__lowerBound">
-          {bigLowerBound.div(10 ** decimals).toFixed(0)} {unit}
-          <div className="slider__lowerBoundLabel">Lower Bound</div>
-        </div>
-        <div className="slider__bar" title="Please enter a value on the right!">
-          <div className="slider__handle" style={{ left: `${percentage.toFixed(4)}%` }}>
-            <div className="slider__handleText">
-              <div className="slider__handleTextLabel">Current Bet</div>
-              <DecimalValue value={value} decimals={decimals} /> {unit}
+    const selectedValue = new Decimal(marginalPriceSelected).mul(bounds.toString()).add(bigLowerBound.div(10 ** decimals).toString())
+    const selectedPercentage = new Decimal(marginalPriceSelected).mul(100)
+    return (
+      <div className="scalarSlider">
+        <div className="scalarSlider__inner">
+          <div className="scalarSlider__lowerBound">
+            {bigLowerBound.div(10 ** decimals).toFixed(0)} {unit}
+            <div className="scalarSlider__lowerBoundLabel">Lower Bound</div>
+          </div>
+          <div className="scalarSlider__bar" title="Please enter a value on the right!">
+            <div className="scalarSlider__handle" style={{ left: `${percentage.toFixed(4)}%` }}>
+              <div className="scalarSlider__handleText">
+                <div className="scalarSlider__handleTextLabel">Current Bet</div>
+                <DecimalValue value={value} decimals={decimals} /> {unit}
+              </div>
+            </div>
+            <div
+              className={cn('scalarSlider__handle scalarSlider__handle--below', {
+                  'scalarSlider__handle--below--pinRight': selectedPercentage.gt(75),
+                  'scalarSlider__handle--below--pinLeft': selectedPercentage.lt(25),
+              })} style={{ left: `${selectedPercentage.toFixed(4)}%` }}
+            >
+              <div className="scalarSlider__handleText">
+                <div className="scalarSlider__handleTextLabel">Selected Bet</div>
+                <DecimalValue value={selectedValue} decimals={decimals} /> {unit}
+              </div>
             </div>
           </div>
-          <div className="slider__handle slider__handle--below" style={{ left: `${selectedPercentage.toFixed(4)}%` }}>
-            <div className="slider__handleText">
-              <div className="slider__handleTextLabel">Selected Bet</div>
-              <DecimalValue value={selectedValue} decimals={decimals} /> {unit}
-            </div>
+          <div className="scalarSlider__upperBound">
+            {bigUpperBound.div(10 ** decimals).toFixed(0)} {unit}
+            <div className="scalarSlider__upperBoundLabel">Upper Bound</div>
           </div>
-        </div>
-        <div className="slider__upperBound">
-          {bigUpperBound.div(10 ** decimals).toFixed(0)} {unit}
-          <div className="slider__upperBoundLabel">Upper Bound</div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
 
 ScalarSlider.propTypes = {
-  lowerBound: PropTypes.number,
-  upperBound: PropTypes.number,
-  unit: PropTypes.string,
-  marginalPriceCurrent: PropTypes.number,
-  marginalPriceSelected: PropTypes.number,
-  decimals: PropTypes.number,
+    lowerBound: PropTypes.number,
+    upperBound: PropTypes.number,
+    unit: PropTypes.string,
+    marginalPriceCurrent: PropTypes.number,
+    marginalPriceSelected: PropTypes.number,
+    decimals: PropTypes.number,
 }
 
 export default ScalarSlider
