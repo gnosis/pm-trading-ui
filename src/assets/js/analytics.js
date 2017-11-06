7d82e73de93ef48338458cd6bcb1befb81fd36af
Mikhail Mikheev <mmvsha73@gmail.com>
2017-11-06 17:10:06 +0400

    move ga to js asset

---
+++ src/assets/js/analytics.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  dataLayer.push(arguments)
+}
+gtag('js', new Date())
+gtag('config', 'UA-83220550-2')
+window.ga =
+  window.ga ||
+  function () {
+    (window.ga.q = window.ga.q || []).push(arguments)
+  }
