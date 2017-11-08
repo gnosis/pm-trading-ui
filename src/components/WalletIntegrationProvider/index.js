618cef78bb4ab63f9b05297211f2e92920f25410
Mikhail Mikheev <mmvsha73@gmail.com>
2017-11-08 16:25:42 +0400

    remove connect wallet modal

--- src/components/WalletIntegrationProvider/index.js
+++ src/components/WalletIntegrationProvider/index.js
@@ -12,101 +12,98 @@ import { weiToEth } from 'utils/helpers'
 const GNOSIS_REINIT_KEYS = ['network', 'account', 'available']
 
 class WalletIntegrationProvider extends Component {
-    constructor(props) {
-        super(props)
-        const { integrations, uportDefaultAccount } = props
+  constructor(props) {
+    super(props)
+    const { integrations, uportDefaultAccount } = props
 
-        const providerOptions = {
-            runProviderUpdate: this.handleProviderUpdate,
-            runProviderRegister: this.handleProviderRegister,
-            uportDefaultAccount,
+    const providerOptions = {
+      runProviderUpdate: this.handleProviderUpdate,
+      runProviderRegister: this.handleProviderRegister,
+      uportDefaultAccount,
+    }
+    window.addEventListener('load', () => {
+      Promise.all(map(integrations, integration => integration.initialize(providerOptions)))
+        .then(this.props.initGnosis, this.props.initGnosis)
+        .catch(this.props.initGnosis)
+    })
+  }
+
+  @autobind
+  async handleProviderUpdate(provider, data) {
+    await this.props.updateProvider({
+      provider: provider.constructor.providerName,
+      ...data,
+    })
+
+    if (this.props.gnosisInitialized) {
+      let requireGnosisReinit = false
+      GNOSIS_REINIT_KEYS.forEach((searchKey) => {
+        if (Object.keys(data).indexOf(searchKey) > -1) {
+          requireGnosisReinit = true
         }
-        window.addEventListener('load', () => {
-            Promise.all(map(integrations, integration => integration.initialize(providerOptions)))
-                .then(
-                    this.props.initGnosis,
-                    this.props.initGnosis,
-                )
-                .catch(this.props.initGnosis)
-        })
+      })
+
+      if (requireGnosisReinit) {
+        // Just in case any other provider is updated and the default one
+        // is UPORT we do not want to scan the code again
+        await this.props.initGnosis()
+      }
+
+      return
     }
 
-    @autobind
-    async handleProviderUpdate(provider, data) {
-        await this.props.updateProvider({
-            provider: provider.constructor.providerName,
-            ...data,
-        })
+    if (provider.constructor.providerName === 'UPORT') {
+      await this.props.initGnosis()
+      const account = (await getGnosisConnection()).defaultAccount
+      const balance = await getOlympiaTokensByAccount(account)
+      await this.props.updateProvider({
+        ...data,
+        provider: provider.constructor.providerName,
+        account,
+        balance: weiToEth(balance),
+      })
 
-        if (this.props.gnosisInitialized) {
-            let requireGnosisReinit = false
-            GNOSIS_REINIT_KEYS.forEach((searchKey) => {
-                if (Object.keys(data).indexOf(searchKey) > -1) {
-                    requireGnosisReinit = true
-                }
-            })
-
-            if (requireGnosisReinit) {
-                // Just in case any other provider is updated and the default one
-                // is UPORT we do not want to scan the code again
-                await this.props.initGnosis()
-            }
-
-            return
-        }
-
-        if (provider.constructor.providerName === 'UPORT') {
-            await this.props.initGnosis()
-            const account = (await getGnosisConnection()).defaultAccount
-            const balance = await getOlympiaTokensByAccount(account)
-            await this.props.updateProvider({
-                ...data,
-                provider: provider.constructor.providerName,
-                account,
-                balance: weiToEth(balance),
-            })
-
-            await this.props.fetchOlympiaUserData(account)
-        }
+      await this.props.fetchOlympiaUserData(account)
     }
+  }
 
-    @autobind
-    async handleProviderRegister(provider, data) {
-        if (provider.constructor.providerName === 'UPORT') {
-            data.account = this.props.uportDefaultAccount
-        }
-        await this.props.registerProvider({
-            provider: provider.constructor.providerName,
-            ...data,
-        })
+  @autobind
+  async handleProviderRegister(provider, data) {
+    if (provider.constructor.providerName === 'UPORT') {
+      data.account = this.props.uportDefaultAccount
     }
+    await this.props.registerProvider({
+      provider: provider.constructor.providerName,
+      ...data,
+    })
+  }
 
-    render() {
-        const { children } = this.props
+  render() {
+    const { children } = this.props
 
-        return children
-    }
+    return children
+  }
 }
 
 WalletIntegrationProvider.propTypes = {
-    children: PropTypes.element,
-    integrations: PropTypes.objectOf(PropTypes.object),
-    gnosisInitialized: PropTypes.bool,
-    registerProvider: PropTypes.func.isRequired,
-    updateProvider: PropTypes.func.isRequired,
-    fetchOlympiaUserData: PropTypes.func.isRequired,
-    initGnosis: PropTypes.func.isRequired,
-    uportDefaultAccount: PropTypes.string,
+  children: PropTypes.element,
+  integrations: PropTypes.objectOf(PropTypes.object),
+  gnosisInitialized: PropTypes.bool,
+  registerProvider: PropTypes.func.isRequired,
+  updateProvider: PropTypes.func.isRequired,
+  fetchOlympiaUserData: PropTypes.func.isRequired,
+  initGnosis: PropTypes.func.isRequired,
+  uportDefaultAccount: PropTypes.string,
 }
 
 const mapStateToProps = state => ({
-    gnosisInitialized: isGnosisInitialized(state),
-    uportDefaultAccount: getUportDefaultAccount(state),
+  gnosisInitialized: isGnosisInitialized(state),
+  uportDefaultAccount: getUportDefaultAccount(state),
 })
 
 export default connect(mapStateToProps, {
-    registerProvider,
-    updateProvider,
-    initGnosis,
-    fetchOlympiaUserData,
+  registerProvider,
+  updateProvider,
+  initGnosis,
+  fetchOlympiaUserData,
 })(WalletIntegrationProvider)
