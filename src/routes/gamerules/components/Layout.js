4beb0f93647b22bb409410cb50041f39500e2522
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-11-14 09:13:29 +0100

    Adding Olympia official video

--- src/routes/gamerules/components/Layout.js
+++ src/routes/gamerules/components/Layout.js
@@ -10,23 +10,22 @@ import DashboardOverview from './DashboardOverview'
 import MakePrediction from './MakePrediction'
 import Profits from './Profits'
 
-/* const videoOpts = {
-    height: '260',
-    width: '520',
-    playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 0,
-    },
+const videoOpts = {
+  height: '260',
+  width: '520',
+  playerVars: { // https://developers.google.com/youtube/player_parameters
+    autoplay: 0,
+  },
 }
-*/
 
 const GameRules = () => (
   <PageFrame width={'750px'} >
     <Block margin="md">
       <Title>GAME GUIDE</Title>
-      {/* <Youtube
-        videoId="0WmUbiMHeSE"
+      <Youtube
+        videoId="36GgFg9CgG8"
         opts={videoOpts}
-      /> */}
+      />
     </Block>
     <SignUp />
     <LogIn />
