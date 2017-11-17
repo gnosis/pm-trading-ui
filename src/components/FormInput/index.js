7d319ab699375bff399fee06fbbaba93ccff6ef4
Mikhail <mmvsha73@gmail.com>
2017-11-17 15:24:51 +0400

    Fix FormInput classnames

--- src/components/FormInput/index.js
+++ src/components/FormInput/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { fieldPropTypes } from 'redux-form'
+import cn from 'classnames'
 
 import { bemifyClassName } from 'utils/helpers'
 
@@ -15,31 +16,37 @@ const Input = ({
   continuousPlaceholder,
   meta: { touched, error },
   ...rest
-}) => (
-  <div className={`inputField ${bemifyClassName(className)}`}>
-    <label htmlFor={input.name} className={`inputField__label ${bemifyClassName(className, 'label')}`}>
-      {label}
-    </label>
-    <input
-      className={`inputField__input ${bemifyClassName(className, 'input')} ${error && touched
-        ? `inputField__input--error ${bemifyClassName(className, 'input', 'error')}`
-        : ''}`}
-      placeholder={placeholder}
-      type={`${type || 'text'}`}
-      {...input}
-      {...rest}
-    />
-    {continuousPlaceholder && (
+}) => {
+  const showErrorMessage = touched && error
+
+  return (
+    <div className={`inputField ${bemifyClassName(className)}`}>
+      <label htmlFor={input.name} className={`inputField__label ${bemifyClassName(className, 'label')}`}>
+        {label}
+      </label>
       <input
-        className={`inputField__continuousPlaceholder ${bemifyClassName(className, 'continuousPlaceholder')}`}
-        type="text"
-        value={continuousPlaceholder}
-        disabled
+        className={cn('inputField__input', {
+          [bemifyClassName(className, 'input')]: className,
+          'inputField__input--error': showErrorMessage,
+          [bemifyClassName(className, 'input', 'error')]: className && showErrorMessage,
+        })}
+        placeholder={placeholder}
+        type={`${type || 'text'}`}
+        {...input}
+        {...rest}
       />
-    )}
-    {touched && error && <span>{error}</span>}
-  </div>
-)
+      {continuousPlaceholder && (
+        <input
+          className={`inputField__continuousPlaceholder ${bemifyClassName(className, 'continuousPlaceholder')}`}
+          type="text"
+          value={continuousPlaceholder}
+          disabled
+        />
+      )}
+      {showErrorMessage && <span>{error}</span>}
+    </div>
+  )
+}
 
 Input.propTypes = {
   ...fieldPropTypes,
