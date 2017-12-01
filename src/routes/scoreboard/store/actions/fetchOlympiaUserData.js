2759a76abe8cdfaeb9b88230a52f15f28a4a59b1
Adolfo Panizo <adolfo.panizo@gmail.com>
2017-12-01 07:51:13 +0100

    PM-268 Using env variable for fetching users data from API

--- src/routes/scoreboard/store/actions/fetchOlympiaUserData.js
+++ src/routes/scoreboard/store/actions/fetchOlympiaUserData.js
@@ -1,14 +1,15 @@
 import { restFetch } from 'utils/helpers'
 import addUsers from './addUsers'
 
-const url = 'https://gnosisdb-olympia.gnosis.pm/api/scoreboard/'
+const API_URL = `${process.env.GNOSISDB_URL}/api`
+const url = `${API_URL}/scoreboard/`
 
 export default account => dispatch =>
-    restFetch(url + account)
-        .then((response) => {
-            if (!response) {
-                dispatch(addUsers([]))
-            }
+  restFetch(url + account)
+    .then((response) => {
+      if (!response) {
+        dispatch(addUsers([]))
+      }
 
-            dispatch(addUsers([response]))
-        })
+      dispatch(addUsers([response]))
+    })
