b2037efde4d4da0f03fcc3cdcf9a58d651a55057
Mikhail <mmvsha73@gmail.com>
2017-12-14 17:43:22 +0400

    Fix heading PM-361

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -250,7 +250,7 @@ class Dashboard extends Component {
     if (marketType === 'closingMarkets') {
       return (
         <div className="dashboardWidget col-md-6" key={marketType}>
-          <div className="dashboardWidget__market-title">Next Markets to be resolved</div>
+          <div className="dashboardWidget__market-title">Closing Next</div>
           <div
             className={cn({
               dashboardWidget__container: true,
