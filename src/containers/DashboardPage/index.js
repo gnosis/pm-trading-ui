ef5cdca6a5cb143f1fe4a0123eaadba5515662e8
Mikhail <mmvsha73@gmail.com>
2017-12-20 17:19:27 +0400

    Revert "conflicts fix"
    This reverts commit 6e437ab29514a3131ccd73cde370165c8990dc2f, reversing
    changes made to 8a030865ca1c54e383003d83799f9b495cc80456.

--- src/containers/DashboardPage/index.js
+++ src/containers/DashboardPage/index.js
@@ -3,7 +3,10 @@ import { push } from 'react-router-redux'
 
 
 import DashboardPage from 'components/Dashboard'
-import { getMarkets } from 'selectors/market'
+import {
+  getAccountPredictiveAssets,
+  getMarkets,
+} from 'selectors/market'
 import {
   getAccountTrades,
 } from 'selectors/marketTrades'
@@ -20,6 +23,7 @@ const mapStateToProps = (state) => {
   const markets = getMarkets(state)
   const defaultAccount = getCurrentAccount(state)
   const accountTrades = getAccountTrades(defaultAccount)(state)
+  const accountPredictiveAssets = weiToEth(getAccountPredictiveAssets(state, defaultAccount))
   const accountShares = getAccountShares(state)
   const gnosisInitialized = isGnosisInitialized(state)
   let etherTokens = getEtherTokensAmount(state, defaultAccount)
@@ -37,6 +41,7 @@ const mapStateToProps = (state) => {
     etherTokens,
     accountShares,
     accountTrades,
+    accountPredictiveAssets,
     gnosisInitialized,
   }
 }
