03c8638aa6ef4d53aebbfb96cd81e17b00a0e5f3
Mikhail <mmvsha73@gmail.com>
2018-02-02 18:11:49 +0400

    fix missing brace in analytics/init.js

--- src/utils/analytics/init.js
+++ src/utils/analytics/init.js
@@ -4,4 +4,4 @@ export default function init (){(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObjec
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-83220550-2', 'auto', 'olympiatracker');
-ga('send', 'pageview');
+ga('send', 'pageview');}
