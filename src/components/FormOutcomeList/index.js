2be7e37610fa00710d52427eb0d11d9072240f73
Mikhail Mikheev <mmvsha73@gmail.com>
2018-02-07 14:01:12 +0400

    Revert "PM-411 Refactor: Remove market creation (#224)" (#225)
    This reverts commit bf1ef1fcc9f1ad7e7d0065d44965898026ba96e2.

---
+++ src/components/FormOutcomeList/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import AddOutcome from './AddOutcome'
+import Outcome from './Outcome'
+
+import './formOutcomeList.scss'
+
+class FormOutcomeList extends Component {
+  addOutcome = (event) => {
+    event.preventDefault()
+    this.props.fields.push('')
+  }
+
+  addOutcomeOnChange = (event) => {
+    const { fields } = this.props
+    const { target } = event
+    const { value } = target
+    const index = +target.getAttribute('data-index')
+
+    // new outcome is added when user started typing inside the last field
+    const addNewOutcome = index === fields.length - 1 && value != null && value.length > 0 && fields.length < 3
+    if (addNewOutcome) {
+      fields.push('')
+    }
+  }
+
+  removeOutcome = (event) => {
+    event.preventDefault()
+    const { fields } = this.props
+    const index = event.target.getAttribute('data-index')
+    fields.remove(index)
+  }
+
+  render() {
+    const { fields, label, meta: { error, invalid } } = this.props
+    const showAddButton = fields.length < 3
+    const showDeleteButton = fields.length > 2
+    const addOutcomeLabel = 'Add'
+    const deleteOutcomeLabel = 'Delete'
+
+    return (
+      <div className="formOutcomeList">
+        <label htmlFor="outcomes" className="formOutcomeList__label">
+          {label}
+        </label>
+        {fields.map((field, index) => (
+          <Outcome
+            key={index}
+            index={index}
+            field={field}
+            showDelete={showDeleteButton}
+            onChange={this.addOutcomeOnChange}
+            deleteLabel={deleteOutcomeLabel}
+            onDeleteClick={this.removeOutcome}
+          />
+        ))}
+        {showAddButton && <AddOutcome text={addOutcomeLabel} onClick={this.addOutcome} />}
+        {invalid && error && <span>{error}</span>}
+      </div>
+    )
+  }
+}
+
+FormOutcomeList.propTypes = {
+  fields: PropTypes.shape({
+    push: PropTypes.func,
+    remove: PropTypes.func,
+  }).isRequired,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool,
+    error: PropTypes.string,
+  }),
+  label: PropTypes.string,
+}
+
+export default FormOutcomeList
