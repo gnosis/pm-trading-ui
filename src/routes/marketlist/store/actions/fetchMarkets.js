8810bd94770fa9f3f6755704b6d28fa6f38bbc99
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-02-08 17:19:17 +0100

    PM-441 Test adding markets to store using Market Builder pattern

--- src/routes/marketlist/store/actions/fetchMarkets.js
+++ src/routes/marketlist/store/actions/fetchMarkets.js
@@ -1,5 +1,7 @@
+import { List } from 'immutable'
 import { requestFromRestAPI } from 'api/utils/fetch'
 import { hexWithoutPrefix } from 'utils/helpers'
+import { BoundsRecord, MarketRecord, OutcomeRecord } from '../models'
 import addMarkets from './addMarkets'
 
 // TODO The default assignment is because JEST test do not work out of the box
@@ -7,13 +9,43 @@ import addMarkets from './addMarkets'
 const whitelisted = process.env.WHITELIST || {}
 const addresses = Object.keys(whitelisted).map(address => hexWithoutPrefix(address))
 
+const buildOutcomesFrom = (outcomes, marginalPrices) => {
+  if (!outcomes) {
+    return List([])
+  }
+
+  const outcomesRecords = outcomes.map((outcome, index) =>
+    new OutcomeRecord({ outcome, marginalPrice: marginalPrices[index] }))
+
+  return List(outcomesRecords)
+}
+
+const buildBoundsFrom = (lower, upper, unit) => BoundsRecord({ lower, upper, unit })
+
+const extractMarkets = markets => markets.map((market) => {
+  const { eventDescription } = market.event.oracle
+  const { title, resolutionDate: date, outcomes: outcomesResponse } = eventDescription
+  const volume = market.tradingVolume
+  const { type } = market.event
+  const outcomes = buildOutcomesFrom(outcomesResponse, market.marginalPrices)
+  const { unit } = market.event.oracle.eventDescription
+  const bounds = buildBoundsFrom(market.event.lowerBound, market.event.upperBound, unit)
+
+  // eslint-disable-next-line
+  const marketRecord = new MarketRecord({ title, date, volume, type, outcomes, bounds });
+
+  return marketRecord
+})
+
+
 export const processMarketResponse = (dispatch, response) => {
   if (!response || !response.results) {
     dispatch(addMarkets([]))
     return
   }
 
-  dispatch(addMarkets(response.results))
+  const marketRecords = extractMarkets(response.results)
+  dispatch(addMarkets(marketRecords))
 }
 
 export default () => dispatch =>
