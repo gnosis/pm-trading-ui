ac227ef8fd50afb2120f5bbbe74ed8d1e69a5f19
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-02-09 09:51:43 +0100

    PM-441 Added test for replace markets after request

--- src/routes/marketlist/store/reducers/market.spec.js
+++ src/routes/marketlist/store/reducers/market.spec.js
@@ -2,9 +2,8 @@ import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { List } from 'immutable'
 import { processMarketResponse } from '../actions/fetchMarkets'
-import { MARKET_CATEGORICAL, MARKET_SCALAR, OutcomeRecord } from '../models'
 import marketReducer, { REDUCER_ID } from './market'
-import aMarket, { realData } from './market.builder'
+import { oneMarketData, twoMarketData, realData, MarketFactory } from './market.builder'
 
 describe('Market List Actions', () => {
   let store
@@ -39,38 +38,6 @@ describe('Market List Actions', () => {
     // GIVEN
     const threeMarketsResponse = realData
 
-    const aKittiesMarket = aMarket()
-      .withTitle('How much will Cryptokitties transactions make up of the entire Ethereum network transactions by December 29th, in the last 1500 blocks?')
-      .withDate('2017-12-30T00:00:00')
-      .withVolume('9523809523809680')
-      .withType(MARKET_SCALAR)
-      .withOutcomes(undefined)
-      .withBounds('1', '20', '%')
-      .get()
-
-    const aEthereumMarket = aMarket()
-      .withTitle('What will the number of Ethereum transactions be on January 3rd, 2018?')
-      .withDate('2018-01-03T12:00:00')
-      .withVolume('857142857142872776')
-      .withType(MARKET_SCALAR)
-      .withOutcomes(undefined)
-      .withBounds('500000', '1250000', 'Txs')
-      .get()
-
-    const gasOutcomes = List.of(
-      new OutcomeRecord({ name: '<20 GWei', marginalPrice: '0.2287' }),
-      new OutcomeRecord({ name: '20 GWei', marginalPrice: '0.3454' }),
-      new OutcomeRecord({ name: '>20 GWei', marginalPrice: '0.4259' }),
-    )
-    const aGasPriceMarket = aMarket()
-      .withTitle('What will be the median gas price on Feb. 1st, 2018?')
-      .withDate('2018-02-01T12:00:00')
-      .withVolume('342952380952380954')
-      .withType(MARKET_CATEGORICAL)
-      .withOutcomes(gasOutcomes)
-      .withBounds(undefined, undefined, undefined)
-      .get()
-
     // WHEN
     processMarketResponse(store.dispatch, threeMarketsResponse)
 
@@ -78,8 +45,27 @@ describe('Market List Actions', () => {
     const firstMarketRecord = store.getState().marketList.get(0)
     const secondMarketRecord = store.getState().marketList.get(1)
     const thirdMarketRecord = store.getState().marketList.get(2)
-    expect(firstMarketRecord).toEqual(aKittiesMarket)
-    expect(secondMarketRecord).toEqual(aEthereumMarket)
-    expect(thirdMarketRecord).toEqual(aGasPriceMarket)
+
+    expect(firstMarketRecord).toEqual(MarketFactory.aKittiesMarket)
+    expect(secondMarketRecord).toEqual(MarketFactory.aEthereumMarket)
+    expect(thirdMarketRecord).toEqual(MarketFactory.aGasPriceMarket)
+  })
+
+  it('replaces markets in store when fetch data', () => {
+    // GIVEN
+    const kittiesResponse = oneMarketData
+    processMarketResponse(store.dispatch, kittiesResponse)
+
+    // WHEN
+    const etherAndGasMarketsResponse = twoMarketData
+    processMarketResponse(store.dispatch, etherAndGasMarketsResponse)
+
+    // THEN
+    const firstMarketRecord = store.getState().marketList.get(0)
+    const secondMarketRecord = store.getState().marketList.get(1)
+
+    expect(store.getState().marketList.size).toEqual(2)
+    expect(firstMarketRecord).toEqual(MarketFactory.aEthereumMarket)
+    expect(secondMarketRecord).toEqual(MarketFactory.aGasPriceMarket)
   })
 })
