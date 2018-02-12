c75f26b6d0c69563fb9a1f60344dc57617774bb7
Mikhail <mmvsha73@gmail.com>
2018-02-12 19:49:48 +0400

    PM-440 refactor selectors which return a Decimal.js object to avoid invalid argument error, add tests for blockhain selectors

--- src/selectors/blockchain.js
+++ src/selectors/blockchain.js
@@ -4,9 +4,9 @@ import Decimal from 'decimal.js'
  * Returns if gnosis.js is initialized or not
  * @param {*} state - redux state
  */
-export const isGnosisInitialized = state => state.blockchain.get('gnosisInitialized')
+export const isGnosisInitialized = state => !!state.blockchain.get('gnosisInitialized')
 
-export const triedToConnect = state => state.blockchain.get('connectionTried')
+export const triedToConnect = state => !!state.blockchain.get('connectionTried')
 
 export const getGasCosts = (state) => {
   const gasCosts = state.blockchain.get('gasCosts')
@@ -22,8 +22,28 @@ export const getGasCosts = (state) => {
 
 export const isGasCostFetched = (state, property) => state.blockchain.getIn(['gasCosts', property]) !== undefined
 
-export const getEtherTokensAmount = (state, account) => new Decimal(state.blockchain.getIn(['etherTokens', account], 0))
+export const getEtherTokensAmount = (state, account) => {
+  const etherTokensAmount = state.blockchain.getIn(['etherTokens', account], 0)
+  let etherTokensDecimal
+  try {
+    etherTokensDecimal = Decimal(etherTokensAmount)
+  } catch (e) {
+    etherTokensDecimal = Decimal(0)
+  }
 
-export const getGasPrice = state => new Decimal(state.blockchain.get('gasPrice').toString())
+  return etherTokensDecimal
+}
+
+export const getGasPrice = (state) => {
+  const gasPrice = state.blockchain.get('gasPrice', 0)
+  let gasPriceDecimal
+  try {
+    gasPriceDecimal = Decimal(gasPrice.toString())
+  } catch (e) {
+    gasPriceDecimal = Decimal(0)
+  }
+
+  return gasPriceDecimal
+}
 
 export const isGasPriceFetched = state => state.blockchain.get('gasPrice') !== undefined
