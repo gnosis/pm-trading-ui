4d02ae1a433a138b622bc9b66de4e3704adf7f35
Mikhail <mmvsha73@gmail.com>
2018-02-15 19:12:30 +0400

    Move market detail render to a separate function PM-442

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,22 +7,20 @@ import MarketDetailPage from 'containers/MarketDetailPage'
 import TransactionsPage from 'containers/TransactionsPage'
 import DashboardPage from 'containers/DashboardPage'
 
+const marketDetailRender = (props) => {
+  if (web3.utils.isAddress(props.match.params.id)) {
+    return <MarketDetailPage {...props} />
+  }
+  return <Redirect to="/markets/list" />
+}
+
 const AppRouter = () => (
   <Switch>
     <Redirect exact from="/" to="/markets/list" />
     <Route exact path="/dashboard" component={DashboardPage} />
     <Route exact path="/transactions" component={TransactionsPage} />
     <Route exact path="/markets/list" component={MarketListPage} />
-    <Route
-      exact
-      path="/markets/:id/:view?/:shareId?"
-      render={(props) => {
-        if (web3.utils.isAddress(props.match.params.id)) {
-          return <MarketDetailPage {...props} />
-        }
-        return <Redirect to="/markets/list" />
-      }}
-    />
+    <Route exact path="/markets/:id/:view?/:shareId?" render={marketDetailRender} />
   </Switch>
 )
 
