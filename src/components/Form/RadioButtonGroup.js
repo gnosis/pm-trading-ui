3c31095b688d13dc0032d2083d932af8bd4c1b80
Andre Meyer <andre.meyer93@gmail.com>
2018-02-15 13:14:53 +0100

    finish radiobutton component/radiobutton group

---
+++ src/components/Form/RadioButtonGroup.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { fieldPropTypes } from 'redux-form'
+
+import classNames from 'classnames/bind'
+
+import RadioButton from './RadioButton'
+import InputError from './InputError'
+
+import styles from './RadioButtonGroup.scss'
+
+const cx = classNames.bind(styles)
+
+const RadioButtonGroup = ({
+  options, input, className, label, meta, meta: { error, touched },
+}) => (
+  <div className={cx('formRadioButtonGroup', className, {
+    error: (touched && error) !== false,
+  })}
+  >
+    <label>{label}</label>
+    {options.map(option => (
+      <RadioButton
+        key={option.value}
+        value={option.value}
+        label={option.label}
+        input={input}
+        meta={meta}
+      />
+    ))}
+    <InputError error={touched && error} />
+  </div>
+)
+
+RadioButtonGroup.propTypes = {
+  input: PropTypes.shape(fieldPropTypes.input).isRequired,
+  meta: PropTypes.shape(fieldPropTypes.meta).isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    label: PropTypes.node,
+  })),
+  label: PropTypes.node,
+  className: PropTypes.string,
+}
+
+RadioButtonGroup.defaultProps = {
+  options: [],
+  label: '',
+  className: '',
+}
+
+export default RadioButtonGroup
