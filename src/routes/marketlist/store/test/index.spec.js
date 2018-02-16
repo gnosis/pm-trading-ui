61965f1bb490fbae21c5beaa99e45d95978e4f1f
apanizo <adolfo.panizo@gmail.com>
2018-02-16 15:50:25 +0100

    PM-443 Placing all store tests in a common place

--- src/routes/marketlist/store/test/index.spec.js
+++ src/routes/marketlist/store/test/index.spec.js
@@ -2,6 +2,9 @@ import marketTests from './market.spec'
 import openMarketTests from './openMarkets.spec'
 
 describe('Market List Selectors', () => {
+  // ACTIONS AND REDUCERS
+
+  // SELECTORS
   marketTests()
   openMarketTests()
 })
