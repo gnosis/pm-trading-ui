93ec758ddc2f52c27a81817c867e0a24a877f1b9
Mikhail Mikheev <mmvsha73@gmail.com>
2018-02-16 20:04:10 +0400

    Feature PM-401: Revise predicted profits calculation (#237)
    * bring buyshares function back PM-400
    
    * PM-401 Update gnosis.js, fix maximum return sign, move selector according to new folder structure
    
    * PM-401 Delete unused helpers/imports, check if market is resolved to return correct predicted profits
    
    * PM-401 remove unused variable
    
    * Bring back metrics section structure from Olympia, fix shares fetch PM-401
    
    * Fix remote provider integration, return lowercase account from selector PM-401
    
    * Inject gnosis.js instance to window, fix export of calcShareWinnings func PM-401
    
    * remove unnecessary .toLowerCase() PM-401
    
    * fix selector path

--- src/selectors/marketShares.js
+++ src/selectors/marketShares.js
@@ -2,7 +2,8 @@ import { createSelector } from 'reselect'
 import Decimal from 'decimal.js'
 import moment from 'moment'
 
-import { add0xPrefix, calcShareWinnings } from 'utils/helpers'
+import { add0xPrefix } from 'utils/helpers'
+import { calcShareWinnings } from 'containers/DashboardPage/store/selectors/utils'
 import { calcLMSRMarginalPrice, calcLMSROutcomeTokenCount } from 'api'
 
 import { getCurrentAccount } from 'integrations/store/selectors'
@@ -83,7 +84,7 @@ const eventSharesSelector = createSelector(getCurrentAccount, getShares, (accoun
   return eventShares
 })
 
-const enhanceShares = (oracles, events, eventDescriptions, eventMarkets, eventShares, account) => {
+const enhanceShares = (oracles, events, eventDescriptions, eventMarkets, eventShares) => {
   const enhancedShares = {}
 
   Object.keys(eventShares).forEach((eventAddress) => {
@@ -106,7 +107,7 @@ const enhanceShares = (oracles, events, eventDescriptions, eventMarkets, eventSh
       const isShareEventResolved = oracle.isOutcomeSet && event.isWinningOutcomeSet
       const isShareMarketClosed =
         market.stage === MARKET_STAGES.MARKET_CLOSED || moment(resolutionDate).isBefore(moment().utc())
-      const shareWinning = isShareEventResolved ? calcShareWinnings(share, market, event, account) : '0'
+      const shareWinning = isShareEventResolved ? calcShareWinnings(share, market, event) : '0'
 
       if (isShareEventResolved && Decimal(shareWinning).eq(0)) {
         return
