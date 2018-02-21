5fb074add935e30b701f5e2bc9f05ca19d82ebba
Mikhail <mmvsha73@gmail.com>
2018-02-21 14:07:39 +0400

    move RootComponent to components/Root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import 'whatwg-fetch'
 import Raven from 'raven-js'
-import RootComponent from 'RootComponent'
+import RootComponent from 'components/root'
 import { initProviders } from 'integrations/store/actions'
 import Decimal from 'decimal.js'
 import React from 'react'
