714aa22f4260358ff59d76f40c2e4c5e4ba98ae9
Andre Meyer <andre.meyer93@gmail.com>
2018-02-27 11:08:42 +0100

    add  formcomponents

---
+++ src/components/Form/OutcomeSelection/OutcomeBar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames/bind'
+
+import { decimalToText } from 'components/DecimalValue'
+import Decimal from 'decimal.js'
+
+import css from './OutcomeBar.mod.scss'
+
+const cx = classNames.bind(css)
+
+const Outcome = ({
+  onSelect,
+  selected,
+  index,
+  label,
+  probability,
+  color,
+}) => {
+  const inputId = `formOutcomeBar_${index}`
+
+  return (
+    <div className={cx('outcome', { selected })}>
+      <label htmlFor={inputId}>
+        <input
+          type="radio"
+          id={inputId}
+          onChange={onSelect}
+          checked={selected}
+          value={index}
+          style={{ backgroundColor: color }}
+        />
+        <div className={cx('wrapper')}>
+          <div
+            className={cx('outcomeBar')}
+            style={{
+              width: `${probability.toFixed(2)}%`,
+              backgroundColor: color,
+            }}
+          />
+          <div className={cx('outcomeDescription')}>
+            <span className={cx('outcomeLabel')}>{label}</span>
+            <span className={cx('outcomeProbability')}>{decimalToText(probability, 2)}%</span>
+          </div>
+        </div>
+      </label>
+    </div>
+  )
+}
+
+Outcome.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+  probability: PropTypes.instanceOf(Decimal).isRequired,
+  index: PropTypes.number.isRequired,
+  selected: PropTypes.bool,
+  color: PropTypes.string,
+  label: PropTypes.string,
+}
+
+Outcome.defaultProps = {
+  label: 'Outcome',
+  selected: false,
+  color: '#fff',
+}
+
+export default Outcome
---
+++ src/components/Form/RadioButton.js
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { fieldPropTypes } from 'redux-form'
+import classNames from 'classnames/bind'
+
+import styles from './RadioButton.mod.scss'
+
+const cx = classNames.bind(styles)
+
+class RadioButton extends PureComponent {
+  handleOnChange = () => {
+    const { input: { onChange, onBlur }, value } = this.props
+    onBlur()
+    return onChange(value)
+  }
+  render() {
+    const {
+      input,
+      meta: { touched, error },
+      value,
+      label,
+      className,
+      light,
+    } = this.props
+
+    const inputId = `formRadioButton_${input.name}_${JSON.stringify(value)}`
+    const isChecked = input && input.value.toString() === value.toString()
+
+    const radioButtonClasses = cx('formRadioButton', className, {
+      error: (touched && error),
+      light,
+    })
+
+    return (
+      <div className={radioButtonClasses}>
+        <label htmlFor={inputId}>
+          <input
+            type="radio"
+            id={inputId}
+            onChange={this.handleOnChange}
+            checked={isChecked}
+            value={value}
+          />
+          {label}
+        </label>
+      </div>
+    )
+  }
+}
+
+RadioButton.propTypes = {
+  input: PropTypes.shape(fieldPropTypes.input).isRequired,
+  meta: PropTypes.shape(fieldPropTypes.meta).isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  label: PropTypes.node.isRequired,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  light: PropTypes.bool,
+}
+
+RadioButton.defaultProps = {
+  className: undefined,
+  light: false,
+}
+
+export default RadioButton
