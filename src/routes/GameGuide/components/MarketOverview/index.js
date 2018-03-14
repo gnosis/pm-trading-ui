9bf9b88a19b3f068df94f1f90a3a62564ccc4612
Mikhail Mikheev <mmvsha73@gmail.com>
2018-03-14 17:33:16 +0400

    Task: PM-543 implement game guide (#260)
    * add gameguide route pm-531
    
    * rename the route pm-431
    
    * fix link and missing package pm-543
    
    * fix unescaped html entities pm-543

---
+++ src/routes/GameGuide/components/MarketOverview/index.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import Block from 'components/layout/Block'
+import Subtitle from 'components/layout/Subtitle'
+import Paragraph from 'components/layout/Paragraph'
+import Img from 'components/layout/Img'
+import Bold from 'components/layout/Bold'
+
+const marketoverview1 = require('routes/GameGuide/assets/marketoverview1.png')
+
+const MarketOverview = () => (
+  <Block margin="xl">
+    <Subtitle>Market Overview</Subtitle>
+    <Block margin="md">
+      <Img src={marketoverview1} width={1140} bordered />
+    </Block>
+    <Paragraph color="medium">
+      Once logged in, you&apos;ll find <Bold>200 OLY tokens</Bold> in your wallet which you can use to make predictions
+      on various topics. OLY is the official play-money token issued for the Olympia tournament.
+    </Paragraph>
+    <Paragraph>
+      Using your OLY tokens, you can participate in prediction markets of your choice via the Markets page. The Markets
+      page will provide you with an overview of all prediction markets. From the sidebar, you can easily filter this
+      preview by market resolution, resolution date, and trading volume. If you’d like to look for a market by title or
+      description, you’re able to do so through the designated data input forms.
+    </Paragraph>
+    <Paragraph>
+      Throughout the tournament, new prediction markets with varying resolution dates will be offered to you so your
+      predictive skills keep being challenged.
+    </Paragraph>
+    <Paragraph>
+      Every two days, you&apos;ll also get a <Bold>top-up of new OLY tokens</Bold> that you can trade to unlock new
+      badges and climb up the scoreboard.
+    </Paragraph>
+  </Block>
+)
+
+export default MarketOverview
