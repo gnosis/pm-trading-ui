9bf91f574ab5e89e7f28952db99cb57213eb7aa2
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-03-19 13:12:09 +0400

    allow markdown in footer pm-432

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
+import Markdown from 'react-markdown'
+import cn from 'classnames/bind'
 import Block from 'components/layout/Block'
 import Paragraph from 'components/layout/Paragraph'
 import { getFooterText } from 'utils/configuration'
+import style from './Footer.mod.scss'
 
+const cx = cn.bind(style)
 const footerText = getFooterText()
 
 const Footer = () =>
   (footerText.length > 1 ? (
     <Block margin="md">
-      <Paragraph center color="medium">
-        {footerText}
-      </Paragraph>
+      <Markdown source={footerText} className={cx('footerContainer')} />
     </Block>
   ) : (
     <div />
