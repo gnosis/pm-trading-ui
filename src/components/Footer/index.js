c3f4fe222fb6cfb1f3e5eb21d169eaed7212189d
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-03-27 14:00:24 +0400

    make footer fixed pm-563

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,20 @@
 import * as React from 'react'
 import Block from 'components/layout/Block'
 import Paragraph from 'components/layout/Paragraph'
+import './Footer.less'
 
 const Footer = () => (
-  <Block margin="md">
-    <Paragraph center color="medium">
-      Trading on prediction markets carry a high degree of financial risk to your capital. Please read our full Risk
-      Disclaimer and Terms of Service before trading
+  <Block className="footer">
+    <Paragraph color="medium">
+      Trading on prediction markets carry a high degree of financial risk to your capital. Please read our full{' '}
+      <a href="/TermsOfService.html" target="_blank" className="footerLink">
+        Risk Disclaimer
+      </a>{' '}
+      and{' '}
+      <a href="/TermsOfService.html" target="_blank" className="footerLink">
+        Terms of Service
+      </a>{' '}
+      before trading
     </Paragraph>
   </Block>
 )
