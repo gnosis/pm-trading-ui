613a6b279a3c7860182b5e0a41ba7704a5be37d4
Andre Meyer <andre.meyer93@gmail.com>
2018-04-09 14:36:38 +0200

    fix missing files

---
+++ src/routes/MarketList/components/NoMarkets.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoMarkets = () => (
+  <div className="marketListContainer">
+    <div className="market no-markets">No Markets available</div>
+  </div>
+)
+
+export default NoMarkets
