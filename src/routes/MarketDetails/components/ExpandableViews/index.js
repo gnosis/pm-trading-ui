0d4ad7282ab1a294dfe9ce3fb4e308b6116d32cb
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-04-19 12:06:23 +0400

    Add a function to show expandables during tournament PM-582

--- src/routes/MarketDetails/components/ExpandableViews/index.js
+++ src/routes/MarketDetails/components/ExpandableViews/index.js
@@ -1,4 +1,6 @@
 import { isMarketClosed, isMarketResolved } from 'utils/helpers'
+import { getProviderConfig, isFeatureEnabled } from 'utils/features'
+import { WALLET_PROVIDER } from 'integrations/constants'
 import MarketBuySharesForm from './MarketBuySharesForm'
 import MarketMySharesForm, { MY_TOKENS } from './MarketMySharesForm'
 import MarketMyTrades from './MarketMyTrades'
@@ -6,6 +8,21 @@ import MarketMyTrades from './MarketMyTrades'
 export const EXPAND_BUY_SHARES = 'buy-shares'
 export const EXPAND_MY_TRADES = 'my-trades'
 export const EXPAND_MY_SHARES = 'my-shares'
+
+const showExpandableTournament = (props) => {
+  if (!isFeatureEnabled('tournmanet')) {
+    return true
+  }
+
+  let showExpandable = false
+  if (getProviderConfig().default === WALLET_PROVIDER.METAMASK && props.defaultAccount && props.mainnetAddress) {
+    showExpandable = true
+  } else if (props.defaultAccount) {
+    showExpandable = true
+  }
+
+  return showExpandable
+}
 
 const expandableViews = {
   [EXPAND_BUY_SHARES]: {
@@ -18,19 +35,20 @@ const expandableViews = {
       props.defaultAccount &&
       props.defaultAccount !== props.market.owner &&
       !isMarketClosed(props.market) &&
-      !isMarketResolved(props.market),
+      !isMarketResolved(props.market) &&
+      showExpandableTournament(props),
   },
   [EXPAND_MY_SHARES]: {
     label: MY_TOKENS,
     className: 'btn btn-default',
     component: MarketMySharesForm,
-    showCondition: props => props.market && props.defaultAccount,
+    showCondition: props => props.market && props.defaultAccount && showExpandableTournament(props),
   },
   [EXPAND_MY_TRADES]: {
     label: 'My Trades',
     className: 'btn btn-default',
     component: MarketMyTrades,
-    showCondition: props => props.market && props.defaultAccount,
+    showCondition: props => props.market && props.defaultAccount && showExpandableTournament(props),
   },
 }
 
