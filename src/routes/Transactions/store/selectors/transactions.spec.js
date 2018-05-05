017b46d071caba28f89ed82f537f1c190a5a3145
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-05-05 16:42:04 +0400

    Fix failing test PM-589

--- src/routes/Transactions/store/selectors/transactions.spec.js
+++ src/routes/Transactions/store/selectors/transactions.spec.js
@@ -1,31 +1,32 @@
-import { transactionSelector } from 'store/selectors/transactions'
+import { transactionSelector } from 'routes/Transactions/store/selectors/transactions'
+import { Map } from 'immutable'
 
 // import { TRANSACTION_STATUS, TRANSACTION_COMPLETE_STATUS } from 'utils/constants'
 
 describe('transactionsSelector', () => {
   test('it should return a transaction object', () => {
     const state = {
-      transactions: {
-        log: {
+      transactions: Map({
+        log: Map({
           test123: {
             id: 'test123',
           },
-        },
-      },
+        }),
+      }),
     }
 
-    expect(transactionSelector(state, 'test123')).toMatchObject(state.transactions.log.test123)
+    expect(transactionSelector(state, 'test123')).toMatchObject(state.transactions.getIn(['log', 'test123']))
   })
 
   test('it should return an empty object for an invalid transaction id', () => {
     const state = {
-      transactions: {
+      transactions: Map({
         log: {
           test123: {
             id: 'test123',
           },
         },
-      },
+      }),
     }
 
     expect(transactionSelector(state, 'test123')).toMatchObject({})
