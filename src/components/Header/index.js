311bea5efd71896b0d6b98d70ba156c3494b73f5
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-05-08 19:12:55 +0400

    Fix bunch of styling bugs PM-602

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -127,14 +127,14 @@ class Header extends Component {
           </div>
           <div className={cx('group', 'left', 'version')}>{version}</div>
           <div className={cx('group', 'left', 'navLinks')}>
+            <NavLink to="/markets/list" activeClassName={cx('active')} className={cx('navLink')}>
+              Markets
+            </NavLink>
             {walletConnected && (
               <NavLink to="/dashboard" activeClassName={cx('active')} className={cx('navLink')}>
                 Dashboard
               </NavLink>
             )}
-            <NavLink to="/markets/list" activeClassName={cx('active')} className={cx('navLink')}>
-              Markets
-            </NavLink>
             {walletConnected && (
               <NavLink to="/transactions" activeClassName={cx('active')} className={cx('navLink')}>
                 Transactions
