c9366372daa0f865d84ae59a7ea4b034c2babdbc
Andre Meyer <andre.meyer93@gmail.com>
2018-05-17 15:05:20 +0200

    PM-619: disable env logging for tests

--- configuration.setup.js
+++ configuration.setup.js
@@ -2,7 +2,7 @@ const configLoader = require('./configuration')
 
 const ENV = 'local'
 
-console.info(`[JEST]: using env configuration: '${ENV}'`)
+// console.info(`[JEST]: using env configuration: '${ENV}'`)
 const { config, interfaceConfig } = configLoader(ENV)
 window.GNOSIS_CONFIG = config
 window.GNOSIS_INTERFACE = interfaceConfig
