73b7e0030aac8176f999a75d10dffeb5e8400284
Andre <andre.meyer93@gmail.com>
2018-05-22 13:57:02 +0200

    Task: Disable Analytics, abstracted into config entries (#348)
    * implement loader pattern for analytics stuff, implemented ga as analytics loader
    
    * reset default config
    
    * more robust check for existing window.ga
    
    * add optional chaining

---
+++ src/utils/analytics/google/index.js
@@ -0,0 +1,32 @@
+/* global ga */
+import { getThirdPartyConfig } from 'utils/features'
+
+export const THIRD_PARTY_ID = 'googleAnalytics'
+
+const config = getThirdPartyConfig(THIRD_PARTY_ID)
+
+const GOOGLE_ANALYTICS_URL = 'https://www.google-analytics.com/analytics.js'
+
+const loadGoogleAnalytics = () => new Promise((resolve) => {
+  const script = document.createElement('script')
+  script.src = GOOGLE_ANALYTICS_URL
+  script.type = 'text/javascript'
+
+  document.head.appendChild(script)
+
+  script.onload = () => {
+    ga('create', '', 'auto', config.name)
+    ga('send', 'pageview')
+
+    resolve()
+  }
+})
+
+export const ga = (...args) =>
+  (window.ga && window.ga.q && window.ga.q(...args)) ||
+  (window.ga && window.ga(...args)) ||
+  (() => {})(...args) // no-op
+
+export const gaSend = (...args) => ga(config.name, ...args)
+
+export default loadGoogleAnalytics
