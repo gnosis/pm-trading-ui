06bc945aad19bc7a6d2066c83085c7806b1670c7
Andre <andre.meyer93@gmail.com>
2018-05-24 12:08:42 +0200

    added new config group: mainnet, default config group is testnet (#366)

--- env_build.sh
+++ env_build.sh
@@ -3,11 +3,8 @@
 if [[ ${PROJECT} == "mainnet" ]]; then
   # mainnet
   if [[ ${TRAVIS_BRANCH} == "master" ]]; then
-    export GNOSIS_ENV=staging;
+    export GNOSIS_ENV=mainnet/staging;
     export NODE_ENV=production;
-  else
-    export GNOSIS_ENV=development;
-    export NODE_ENV=development;
   fi
 elif [[ ${PROJECT} == "olympia" ]]; then
   # olympia
@@ -16,6 +13,15 @@ elif [[ ${PROJECT} == "olympia" ]]; then
     export NODE_ENV=production;
   else
     export GNOSIS_ENV=olympia/development;
+    export NODE_ENV=development;
+  fi
+else
+  # testnet
+  if [[ ${TRAVIS_BRANCH} == "master" ]]; then
+    export GNOSIS_ENV=staging;
+    export NODE_ENV=production;
+  else
+    export GNOSIS_ENV=development;
     export NODE_ENV=development;
   fi
 fi
