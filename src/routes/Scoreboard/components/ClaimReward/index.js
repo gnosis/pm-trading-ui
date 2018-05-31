e455b03359c4bd0c180b5f32222d870a21951586
Mikhail Mikheev <mmvsha73@gmail.com>
2018-05-31 19:07:48 +0400

    Development (#386)
    * Bug PM-630: Header/Dashboard shows 0 OLY balance (#383)
    
    * Configure olympia for testing PM-651
    
    * Fix travis config PM-651
    
    * Update build env script for olympia testing env PM-651
    
    * Add missing bracket pm-651
    
    * Add mini-css-extract-plugin PM-651
    
    * Update bootstrap-loader settings PM-651
    
    * Update webpack configuration PM-651
    
    * Update whitelist PM-651
    
    * Convert token balance to a string PM-630
    
    * use tokenAmountString PM-630
    
    * call .toString in api call PM-630
    
    * Fix the condition for showing reward claim box PM-653 (#385)

--- src/routes/Scoreboard/components/ClaimReward/index.js
+++ src/routes/Scoreboard/components/ClaimReward/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames/bind'
+import moment from 'moment'
 import Decimal from 'decimal.js'
 import Block from 'components/layout/Block'
 import Span from 'components/layout/Span'
@@ -19,7 +20,9 @@ const ClaimReward = ({ openClaimRewardModal, rewardValue }) => (
   <Block className={cx('claimReward')}>
     <Block className={cx('rewardInfoContainer')}>
       <Block className={cx('rewardAmount')}>
-        <Span className={cx('infoText', 'amount')}>{rewardsClaimed || !rewardValue ? 'N/A' : `${rewardValue} ${rewardToken.symbol}`}</Span>
+        <Span className={cx('infoText', 'amount')}>
+          {rewardsClaimed || !rewardValue ? 'N/A' : `${rewardValue} ${rewardToken.symbol}`}
+        </Span>
         <Paragraph className={cx('annotation')}>CLAIMABLE {rewardToken.symbol}</Paragraph>
       </Block>
       <Block className={cx('timeToClaim')}>
@@ -31,7 +34,15 @@ const ClaimReward = ({ openClaimRewardModal, rewardValue }) => (
         <Paragraph className={cx('annotation')}>TIME LEFT TO CLAIM</Paragraph>
       </Block>
     </Block>
-    <button className={cx('claimButton')} onClick={openClaimRewardModal} disabled={rewardsClaimed || Decimal(rewardValue || 0).lte(0)}>
+    <button
+      className={cx('claimButton')}
+      onClick={openClaimRewardModal}
+      disabled={
+        rewardsClaimed ||
+        Decimal(rewardValue || 0).lte(0) ||
+        !moment.utc().isBetween(claimReward.claimStart, claimReward.claimUntil)
+      }
+    >
       CLAIM NOW
     </button>
   </Block>
