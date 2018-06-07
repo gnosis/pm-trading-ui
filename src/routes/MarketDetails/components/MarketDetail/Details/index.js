e9e30997cb34fe24df19c78832b2ec1ec575fe41
Mikhail Mikheev <mdoublemmm@gmail.com>
2018-06-07 16:06:34 +0400

    Add markdown viewer to market's description PM-648

--- src/routes/MarketDetails/components/MarketDetail/Details/index.js
+++ src/routes/MarketDetails/components/MarketDetail/Details/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import cn from 'classnames/bind'
 import PropTypes from 'prop-types'
 import ImmutableProptypes from 'react-immutable-proptypes'
+import Markdown from 'react-markdown'
 import moment from 'moment'
 import Decimal from 'decimal.js'
 import { LOWEST_VALUE } from 'utils/constants'
@@ -43,11 +44,23 @@ const Details = ({
     className: cx('outcomes'),
   }
 
+  /* eslint-disable */
+  const markdownRenderers = {
+    link: props => (
+      <a href={props.href} target="_blank" rel="noopener noreferrer">
+        {props.children}
+      </a>
+    ),
+  }
+  /* eslint-enable */
+
   return (
     <div className={cx('col-xs-10 col-xs-offset-1 col-sm-9 col-sm-offset-0')}>
-      <div className={cx('marketDescription')}>
-        <p className={cx('text')}>{market.eventDescription.description}</p>
-      </div>
+      <Markdown
+        className={cx('marketDescription')}
+        source={market.eventDescription.description}
+        renderers={markdownRenderers}
+      />
       <Outcome
         resolved={marketResolved}
         type={market.event.type}
