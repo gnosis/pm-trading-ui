9348d798c3317331f5ec94cb102ae06309df25c2
Mikhail Mikheev <mmvsha73@gmail.com>
2018-06-14 13:37:16 +0400

    Bug PM-672: Balance doesn't update after buy/sell/redeem (#407)
    * Fetch collateral token balance after buying/selling
    
    * fetch balance after redeeming winnings PM-672
    
    * Eslint fixes PM-672
    
    * Move requestCollateralTokenBalance to store/actions/blockchain PM-672

--- src/routes/MarketDetails/store/actions/sellShares.js
+++ src/routes/MarketDetails/store/actions/sellShares.js
@@ -2,6 +2,7 @@ import allowedRangePrice from 'utils/marginPrice'
 import uuid from 'uuid/v4'
 import * as api from 'api'
 import Decimal from 'decimal.js'
+import { requestCollateralTokenBalance } from 'store/actions/blockchain'
 import { startLog, closeLog, closeEntrySuccess, closeEntryError } from 'routes/Transactions/store/actions/transactions'
 import { openModal, closeModal } from 'store/actions/modal'
 import { gaSend } from 'utils/analytics/google'
@@ -71,6 +72,7 @@ const sellMarketShares = (market, share, outcomeTokenCount, earnings) => async (
         .toString(),
     },
   }))
+  await dispatch(requestCollateralTokenBalance(currentAccount))
 
   return dispatch(closeLog(transactionId, TRANSACTION_COMPLETE_STATUS.NO_ERROR))
 }
