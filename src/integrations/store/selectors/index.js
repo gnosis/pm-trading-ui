9f4f26857df9848d383cd63849e86f12df2a6b89
Andre Meyer <andre.meyer93@gmail.com>
2018-06-27 10:57:12 +0200

    remove log

--- src/integrations/store/selectors/index.js
+++ src/integrations/store/selectors/index.js
@@ -46,7 +46,6 @@ export const getCurrentAccount = (state) => {
 }
 
 export const hasAcceptedTermsAndConditions = (state) => {
-  console.log(legalComplianceEnabled)
   if (!legalComplianceEnabled) {
     return true
   }
