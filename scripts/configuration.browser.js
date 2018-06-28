2f549c22c58c63336f8ed84f3df48f54d8b16bbc
Andre Meyer <andre.meyer93@gmail.com>
2018-06-28 14:00:22 +0200

    remove console.log

--- scripts/configuration.browser.js
+++ scripts/configuration.browser.js
@@ -5,12 +5,10 @@ const path = require('path')
 const GNOSIS_ENV = process.argv.splice(-1)[0]
 const { GNOSIS_CONFIG } = process.env
 
-console.log({ GNOSIS_ENV })
-
 const configLoader = require('./configuration.js')
 
 const applicationConfiguration = configLoader(GNOSIS_ENV, GNOSIS_CONFIG)
 
 const configTarget = path.join(__dirname, '..', 'dist', 'config.js')
 fs.writeFileSync(configTarget, `window__GNOSIS_CONFIG__ = ${JSON.stringify(applicationConfiguration)}`)
-console.log('Config for env written to dist/config.js')
+console.log('[PM-UI] Config for env written to dist/config.js')
