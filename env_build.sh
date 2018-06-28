ab17618a65d1314e6f8d974db5b48b80fa7ef998
Giacomo Licari <giacomo@MacBook-Pro-di-Giacomo.local>
2018-06-28 14:46:54 +0200

    Add releases deploy based on tags

--- env_build.sh
+++ env_build.sh
@@ -2,13 +2,13 @@
 
 if [[ ${PROJECT} == "mainnet" ]]; then
   # mainnet
-  if [[ ${TRAVIS_BRANCH} == "master" ]]; then
+  if [[ ${TRAVIS_BRANCH} == "master" ]] || [[ ${TRAVIS_BRANCH} == ${TRAVIS_TAG} ]]; then
     export GNOSIS_ENV=mainnet/staging;
     export NODE_ENV=production;
   fi
 elif [[ ${PROJECT} == "olympia" ]]; then
   # olympia
-  if [[ ${TRAVIS_BRANCH} == "master" ]] || [[ ${TRAVIS_BRANCH} == "olympia-qa" ]]; then
+  if [[ ${TRAVIS_BRANCH} == "master" ]] || [[ ${TRAVIS_BRANCH} == "olympia-qa" ]] || [[ ${TRAVIS_BRANCH} == ${TRAVIS_TAG} ]]; then
     export GNOSIS_ENV=olympia/staging;
     export NODE_ENV=production;
   else
