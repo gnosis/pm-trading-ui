f16c9284db770b8a954f9cc57ad04ba3f01cc91d
Andre <andre.meyer93@gmail.com>
2018-06-28 15:40:08 +0200

    Task PM-656: Configuration at Runtime (#424)
    * refactor configurations, implement config loader for browser
    
    * fix builds, implement in webpack, implement prod build
    
    * remove console.log
    
    * fix build
    
    * update clean command in package.json

--- src/utils/features.js
+++ src/utils/features.js
@@ -1,23 +1,42 @@
-const config = window.GNOSIS_CONFIG
-const configInterface = window.GNOSIS_INTERFACE
+/* globals FALLBACK_CONFIG */
+/* eslint-disable no-underscore-dangle */
+
+/* How configuration loading works:
+ * - FALLBACK_CONFIG is defined as a Webpack Define (needs to be encoded because it gets replaced in code)
+ * - window.__GNOSIS_CONFIG__ gets injected by config.js in dist after build
+ *
+ * Fallback Config is used for development and production builds, but as soon as /config.js becomes available on
+ * production servers, the external configuration specified in config.js is used. This allows us
+ * to define the configuration after building and it allows to be easily interchanged, when config.js gets changed.
+ */
+const fallbackConfigEncoded = process.env.FALLBACK_CONFIG
+
+let fallbackConfig
+if (fallbackConfigEncoded) {
+  fallbackConfig = JSON.parse(atob(fallbackConfigEncoded))
+}
+const externalConfig = window.__GNOSIS_CONFIG__
+
+window.__GNOSIS_CONFIG_FALLBACK__ = fallbackConfig
+
+const config = externalConfig || fallbackConfig
 
 export const getConfiguration = () => config
-export const getInterfaceConfiguration = () => configInterface
 
-export const getLogoConfig = () => configInterface.logo
+export const getLogoConfig = () => config.logo
 
 export const getGasPriceConfig = () => config.gasPrice || {}
 
-export const isFeatureEnabled = feature => configInterface[feature] && configInterface[feature].enabled
+export const isFeatureEnabled = feature => config[feature] && config[feature].enabled
 
-export const getFeatureConfig = feature => configInterface[feature] && configInterface[feature]
+export const getFeatureConfig = feature => config[feature] && config[feature]
 
-export const getProviderConfig = () => configInterface.providers
+export const getProviderConfig = () => config.providers
 
 export const getProviderIntegrationConfig = providerName =>
-  (configInterface.providers &&
-  configInterface.providers.options &&
-  configInterface.providers.options[providerName.toUpperCase()]) || {}
+  (config.providers &&
+    config.providers.options &&
+    config.providers.options[providerName.toUpperCase()]) || {}
 
 export const isThirdPartyIntegrationEnabled = thirdPartyName =>
   config.thirdparty?.[thirdPartyName]?.enabled === true
@@ -25,4 +44,4 @@ export const isThirdPartyIntegrationEnabled = thirdPartyName =>
 export const getThirdPartyConfig = thirdPartyName =>
   (isThirdPartyIntegrationEnabled(thirdPartyName) ? config.thirdparty[thirdPartyName] : {})
 
-export const getConstant = constantName => configInterface.constants?.[constantName]
+export const getConstant = constantName => config.constants?.[constantName]
