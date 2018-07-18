1b96839bdecd894bf8625f04db4fbdb6cc53518e
Mikhail Mikheev <mmvsha73@gmail.com>
2018-07-18 18:41:55 +0400

    Add commas for bounds

--- src/routes/MarketDetails/components/ExpandableViews/MarketBuySharesForm/OutcomesSection/OutcomeSectionScalar/ScalarSlider/index.js
+++ src/routes/MarketDetails/components/ExpandableViews/MarketBuySharesForm/OutcomesSection/OutcomeSectionScalar/ScalarSlider/index.js
@@ -32,42 +32,26 @@ const ScalarSlider = ({
     <div className={cx('scalarSlider')}>
       <div className={cx('inner')}>
         <div className={cx('lowerBound')}>
-          <div className={cx('boundValue')}>
-            {`${bigLowerBound.div(10 ** decimals).toFixed(0)} ${unit}`}
-          </div>
-          <div className={cx('boundLabel')}>
-            Lower Bound
-          </div>
+          <div className={cx('boundValue')}>{`${parseInt(lowerBound, 10).toLocaleString()} ${unit}`}</div>
+          <div className={cx('boundLabel')}>Lower Bound</div>
         </div>
         <div className={cx('bar')} title="Please enter a value on the right!">
           <div className={cx('sliderHandle')} style={currentValueSliderStyle}>
             <div className={cx('handleText')}>
-              <div className={cx('handleTextLabel')}>
-                Predicted Outcome
-              </div>
-              <div className={cx('handleTextValue')}>
-                {`${decimalToText(value.toFixed(displayDecimals))} ${unit}`}
-              </div>
+              <div className={cx('handleTextLabel')}>Predicted Outcome</div>
+              <div className={cx('handleTextValue')}>{`${decimalToText(value.toFixed(displayDecimals))} ${unit}`}</div>
             </div>
           </div>
           <div className={selectedHandleStyle} style={selectedValueSliderStyle}>
             <div className={cx('handleText')}>
-              <div className={cx('handleTextLabel')}>
-                Selected Trade
-              </div>
-              <div className={cx('handleTextValue')}>
-                {`${decimalToText(selectedValue)} ${unit}`}
-              </div>
+              <div className={cx('handleTextLabel')}>Selected Trade</div>
+              <div className={cx('handleTextValue')}>{`${decimalToText(selectedValue)} ${unit}`}</div>
             </div>
           </div>
         </div>
         <div className={cx('upperBound')}>
-          <div className={cx('boundValue')}>
-            {`${bigUpperBound.div(10 ** decimals).toFixed(0)} ${unit}`}
-          </div>
-          <div className={cx('boundLabel')}>
-            Upper Bound
-          </div>
+          <div className={cx('boundValue')}>{`${parseInt(upperBound, 10).toLocaleString()} ${unit}`}</div>
+          <div className={cx('boundLabel')}>Upper Bound</div>
         </div>
       </div>
     </div>
