28c3a545f810a2054416b76ebd1c73c3213cce29
Mikhail Mikheev <mmvsha73@gmail.com>
2018-07-30 15:14:12 +0400

    Bring back window.__GNOSIS_CONFIG__ for jest

--- scripts/configuration.jest.js
+++ scripts/configuration.jest.js
@@ -4,4 +4,6 @@ const ENV = 'local'
 
 // console.info(`[JEST]: using env configuration: '${ENV}'`)
 const config = configLoader(ENV)
+
+window.__GNOSIS_CONFIG__ = config
 global.__GNOSIS_CONFIG__ = config
