bd3d440f76280980ad4cc5eaafa3afc15f94c88c
Mikhail Mikheev <mmvsha73@gmail.com>
2018-08-13 16:35:24 +0400

    Use react-transition-group PM-720

--- src/components/CookieBanner/index.js
+++ src/components/CookieBanner/index.js
@@ -2,57 +2,73 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames/bind'
 import autobind from 'autobind-decorator'
+import CSSTransition from 'react-transition-group/CSSTransition'
+import { getCookie, setCookie } from './utils'
 import style from './style.scss'
 
 const cx = cn.bind(style)
 
 class CookieBanner extends Component {
   state = {
-    shouldHide: false,
-    shouldShow: false,
+    shown: false,
   }
 
   componentDidMount() {
-    const { display } = this.props
-    if (display) {
-      this.setState({ shouldShow: true })
+    const { display, options } = this.props
+
+    const noCookiesSet = !options.filter(({ label }) => getCookie(label) !== '').length
+    const shouldShow = display || noCookiesSet
+
+    if (shouldShow) {
+      this.setState({ shown: true })
     }
   }
 
   @autobind
   handleClose() {
     this.setState({
-      shouldHide: true,
-      shouldShow: false,
+      shown: false,
     })
   }
 
+  @autobind
+  handleOptionClick() {}
+
   render() {
     const { options, selected } = this.props
-    const { shouldHide, shouldShow } = this.state
+    const { shown } = this.state
+
+    const animationClassNames = {
+      enter: cx('float-enter'),
+      enterActive: cx('float-enter-active'),
+      exit: cx('float-exit'),
+      exitActive: cx('float-exit-active'),
+    }
 
     return (
-      <div className={cx('cookieBar', { shown: shouldShow }, { hidden: shouldHide })}>
-        <p>
-          We use cookies to give you the best experience and to help improve our website. Please read our{' '}
-          <a href="/cookies">Cookie Policy</a> for more information. By clicking &quot;Accept Cookies,&quot; you agree
-          to the storing of cookies on your device to enhance site navigation and analyze site usage.
-        </p>
-        <div className={cx('settings')}>
-          <span className={cx('options')}>
-            {options.map(({ label }) => (
-              <div key={label} className={cx('checkbox')}>
-                <input id="cb-1" name="neccesary" type="checkbox" checked={selected.indexOf(label) > -1} />
-                <label htmlFor="cb-1">{label}</label>
-              </div>
-            ))}
-          </span>
-          <button type="button" id="{config.banner.accept_button}" className={cx('button', 'accept')}>
-            Accept Cookies
-          </button>
+      <CSSTransition in={shown} classNames={animationClassNames} timeout={300} unmountOnExit>
+        <div className={cx('cookieBar')}>
+          <p>
+            We use cookies to give you the best experience and to help improve our website. Please read our{' '}
+            <a href="/cookies">Cookie Policy</a> for more information. By clicking &quot;Accept Cookies,&quot; you agree
+            to the storing of cookies on your device to enhance site navigation and analyze site usage.
+          </p>
+          <div className={cx('settings')}>
+            <span className={cx('options')}>
+              {options.map(({ label }) => (
+                <div key={label} className={cx('checkbox')}>
+                  <input name={label} type="checkbox" checked={selected.indexOf(label) > -1} />
+                  <label htmlFor="cb-1">{label}</label>
+                </div>
+              ))}
+            </span>
+            <button type="button" id="{config.banner.accept_button}" className={cx('button', 'accept')}>
+              Accept Cookies
+            </button>
+          </div>
+          <div className={cx('closeButton')} onClick={this.handleClose} />
         </div>
-        <div className={cx('closeButton')} onClick={this.handleClose} />
-      </div>
+      </CSSTransition>
     )
   }
 }
