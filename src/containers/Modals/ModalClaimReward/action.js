9292fee6b63cfd4df20b7d02e1736cb6653e0ab6
Mikhail Mikheev <mmvsha73@gmail.com>
2018-09-05 14:39:21 +0400

    Hotfix: reward claiming

--- src/containers/Modals/ModalClaimReward/action.js
+++ src/containers/Modals/ModalClaimReward/action.js
@@ -4,7 +4,7 @@ import { getFeatureConfig } from 'utils/features'
 import { calcClaimRewardGasCost } from './api'
 
 const contractType = GAS_COST.CLAIM_REWARD
-const { claimReward } = getFeatureConfig('rewards')
+const { claimReward } = getFeatureConfig('rewardClaiming')
 
 const requestClaimRewardGasCost = () => async (dispatch) => {
   const gasCost = await calcClaimRewardGasCost(claimReward.contractAddress)
