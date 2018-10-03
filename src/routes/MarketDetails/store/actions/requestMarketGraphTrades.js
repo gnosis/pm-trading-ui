2fc73d57d5ac31c050438985a9a7b9e426a505f9
Mikhail Mikheev <mmvsha73@gmail.com>
2018-10-03 18:15:09 +0400

    Trades fetch algorithm WIP PM-777

--- src/routes/MarketDetails/store/actions/requestMarketGraphTrades.js
+++ src/routes/MarketDetails/store/actions/requestMarketGraphTrades.js
@@ -4,29 +4,63 @@ import { hexWithoutPrefix } from 'utils/helpers'
 import { requestFromRestAPI } from 'api/utils/fetch'
 import { processTradesResponse } from 'store/actions/trades'
 
+const TOTAL_TRADES_LIMIT = 2000
+
+const getRequestParameters = (from, to, order = 'ASC', limit = 200, offset = 0) => ({
+  creation_date_time_0: moment.utc(from).format('YYYY-MM-DD hh:mm:ss'),
+  creation_date_time_1: moment.utc(to).format('YYYY-MM-DD hh:mm:ss'),
+  ordering: order === 'ASC' ? 'creation_date_order' : '-creation_date_order',
+  limit,
+  offset,
+})
+
 export default ({ creation, resolution, address }) => async (dispatch) => {
   if (!address || !creation || !resolution) {
     return
   }
 
   const normalizedMarket = hexWithoutPrefix(address)
-  const diff = moment(resolution).diff(moment(creation), 'days')
   const trades = []
 
-  // fetch latest 15 trades of an each day the market has been running
-  for (let i = 0; i <= diff; i += 1) {
-    const requestParameters = {
-      creation_date_time_0: moment(creation)
-        .add(i, 'days')
-        .format('YYYY-MM-DD hh:mm:ss'),
-      creation_date_time_1: moment(creation)
-        .add(i + 1, 'days')
-        .format('YYYY-MM-DD hh:mm:ss'),
-      ordering: '-creation_date_order',
-      limit: 15,
-    }
+  const { count } = await requestFromRestAPI(
+    `markets/${normalizedMarket}/trades/`,
+    getRequestParameters(creation, resolution, 'ASC', 1),
+  )
 
-    trades.push(requestFromRestAPI(`markets/${normalizedMarket}/trades/`, requestParameters))
+  // fetch all trades if they're in limit
+  if (count <= TOTAL_TRADES_LIMIT) {
+    for (let i = 0; i < count; i += 200) {
+      trades.push(
+        requestFromRestAPI(
+          `markets/${normalizedMarket}/trades/`,
+          getRequestParameters(creation, resolution, 'ASC', 200, i),
+        ),
+      )
+    }
+  } else {
+    const resolutionDatePassed = moment.utc().isBefore(moment.utc(resolution))
+    const diff = moment
+      .utc(resolutionDatePassed ? moment.utc() : resolution)
+      .diff(moment.utc(creation), 'days')
+    const dailyLimit = 2000 / diff
+
+    // fetch latest *dailyLimit* trades of an each day the market has been running
+    for (let i = 0; i <= diff; i += 1) {
+      const requestParameters = {
+        creation_date_time_0: moment
+          .utc(creation)
+          .add(i, 'days')
+          .format('YYYY-MM-DD hh:mm:ss'),
+        creation_date_time_1: moment
+          .utc(creation)
+          .add(i + 1, 'days')
+          .format('YYYY-MM-DD hh:mm:ss'),
+        ordering: '-creation_date_order',
+        limit: 200,
+      }
+
+      trades.push(requestFromRestAPI(`markets/${normalizedMarket}/trades/`, requestParameters))
+    }
   }
 
   const responses = await Promise.all(trades)
