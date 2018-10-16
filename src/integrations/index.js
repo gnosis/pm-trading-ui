387c8cc5ed1e88d1979f083885474b3db530415c
Andre Meyer <andre.meyer93@gmail.com>
2018-10-16 11:14:08 +0200

    clean up and documented some stuff

--- src/integrations/index.js
+++ src/integrations/index.js
@@ -1,32 +1,16 @@
-import { isFeatureEnabled, getProviderConfig } from 'utils/features'
-import Metamask from './metamask'
-import Parity from './parity'
-import Remote from './remote'
 import { WALLET_PROVIDER } from './constants'
-
-const providersInstances = [Metamask, Parity, Remote]
 
 // eslint-disable-next-line
 let providers = {}
 
-providersInstances.forEach((instance) => {
-  providers[instance.constructor.providerName] = instance
-})
-
-const tournament = isFeatureEnabled('tournament')
-const providerConfig = getProviderConfig()
-
-if (tournament) {
-  const provider = providerConfig.default
-  if (provider) {
-    if (provider === WALLET_PROVIDER.METAMASK) {
-      providers = { [WALLET_PROVIDER.METAMASK]: Metamask, [WALLET_PROVIDER.REMOTE]: Remote }
-    } else if (provider === WALLET_PROVIDER.UPORT) {
-      // eslint-disable-next-line
-      const Uport = require('integrations/uport')
-      providers = { [WALLET_PROVIDER.UPORT]: Uport, [WALLET_PROVIDER.REMOTE]: Remote }
-    }
+Object.keys(WALLET_PROVIDER).forEach((integrationName) => {
+  // eslint-disable-next-line
+  const providerInstance = require(`./${integrationName.toLowerCase()}/index.js`).default
+  if (providerInstance != null) {
+    providers[providerInstance.constructor.providerName] = providerInstance
+  } else {
+    throw new Error(`Could not load provider ${integrationName}`)
   }
-}
+})
 
 export default providers
