21d271c751f2255fadb3d4636feb0d77fb9f6055
Mikhail Mikheev <mmvsha73@gmail.com>
2018-11-01 19:07:48 +0400

    lazy load mobile header

--- src/components/Header/Layouts/index.js
+++ src/components/Header/Layouts/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Loadable from 'react-loadable'
 import { Desktop, Tablet, Mobile } from 'components/Responsive'
-import MobileHeader from './MobileHeader'
 
 const LoadableDesktop = Loadable({
   loader: () => import('./DesktopHeader'),
@@ -22,7 +21,7 @@ const Layout = props => (
       <LoadableMobile {...props} />
     </Tablet>
     <Mobile>
-      <MobileHeader {...props} />
+      <LoadableMobile {...props} />
     </Mobile>
   </>
 )
