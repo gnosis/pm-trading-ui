296166852ff57c7126e16aa6d96b0139faa44774
Andre Meyer <andre.meyer93@gmail.com>
2018-11-08 18:16:42 +0100

    finish translations on marketlist page

--- src/routes/MarketList/components/MarketsTitle/index.js
+++ src/routes/MarketList/components/MarketsTitle/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withNamespaces } from 'react-i18next'
 import classNames from 'classnames/bind'
 
 import css from './MarketsTitle.scss'
 
 const cx = classNames.bind(css)
 
-const MarketListTitle = () => (
+const MarketListTitle = ({ t }) => (
   <div className={cx('marketListTitle')}>
     <div className="container">
-      <h1>Market overview</h1>
+      <h1>{t('markets.list_title')}</h1>
     </div>
   </div>
 )
 
-export default MarketListTitle
+MarketListTitle.propTypes = {
+  t: PropTypes.func.isRequired,
+}
+
+export default withNamespaces()(MarketListTitle)
