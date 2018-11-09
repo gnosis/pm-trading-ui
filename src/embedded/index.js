c332a37645e09d6ffb95b37f10a1c384ad2aedd2
Mikhail Mikheev <mmvsha73@gmail.com>
2018-11-09 17:30:58 +0400

    Styles fix for embedded view

--- src/embedded/index.js
+++ src/embedded/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import 'normalize.css'
-import './style/embedded.css'
+import './style/embedded.css?raw'
 
 import { setMomentRelativeTime, setMomentDurationFormat } from '../setup'
 
