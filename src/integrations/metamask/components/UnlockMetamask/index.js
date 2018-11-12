8498f76bdb2a0b145748116448814cdaea00d2b5
Mikhail Mikheev <mmvsha73@gmail.com>
2018-11-12 16:09:47 +0400

    eslint fix

--- src/integrations/metamask/components/UnlockMetamask/index.js
+++ src/integrations/metamask/components/UnlockMetamask/index.js
@@ -16,7 +16,7 @@ const tournamentConfig = getFeatureConfig('tournament')
 
 const UnlockMetamask = ({ closeModal }) => (
   <div className={cx('unlockMetamask')}>
-    <button className={cx('closeButton')} onClick={closeModal} />
+    <button type="button" className={cx('closeButton')} onClick={closeModal} />
     <img src={MetamaskIcon} alt="logo" style={logoStyle} />
     <h3 className={cx('heading')}>Unlock your MetaMask wallet</h3>
     <p className={cx('text')}>
